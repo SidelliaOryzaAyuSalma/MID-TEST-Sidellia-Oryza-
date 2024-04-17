d4ae68e41c562f288db84caf24d9e15eb18e66d2
SidelliaOryzaAyuSalma <165445931+SidelliaOryzaAyuSalma@users.noreply.github.com>
2024-04-17 20:51:42 +0700

    Add files via upload

---
+++ SQLite.sql
@@ -0,0 +1,12 @@
+CREATE TABLE employee_table (
+    employee TEXT NOT NULL,
+    salary INTEGER
+);
+
+INSERT INTO employee_table (employee, salary) VALUES
+('Alice', 11),
+('Benn', 43),
+('Charles', 33),
+('Dorothy', 55),
+('Emma', 22),
+('Franklin', 33);
